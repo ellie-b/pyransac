3f11f4ceb11384ee614ce8cee4ca72ac70cc669e
Eleanor Bash <earbash@ucalgary.ca>
2020-08-11 16:42:44 -0600

    Adjusted to reflect RANSAC filter for point cloud alignment based on NN

--- pyransac/base.py
+++ pyransac/base.py
@@ -4,26 +4,29 @@ This module contains definitions for base classes.
 """
 
 # Standard library imports
-import abc
-from typing import List
+import open3d as o3d
 
+def make_transform(self, sample_ind, source, target) -> None:
+    """Makes a model from given data points.
 
-class Model(abc.ABC):
-    """ABC class for data models.
-
-    Derivative classes should extend this class and implement its
-    interface.
+    :param sample_ind: indices for random sample to compute transformation
+    :param source: SurfacePointCloud class from keypts.py
+    :param target: SurfacePointCloud class from keypts.py
     """
-    @abc.abstractmethod
-    def make_model(self, points: List) -> None:
-        """Makes a model from given data points.
+    t_source = source.point_cloud.select_down_sample(sample_ind)
+    t_target = target.point_cloud.select_down_sample(sample_ind)
+    Tmatrix = o3d.registration.TransformationEstimationPointToPoint.compute_transformation(t_source,t_target,with_scaling = True)
+    return Tmatrix
 
-        :param points: list of data points with which to make model
-        """
+def calc_neighbors(self, src_mod,trg_pts) -> float:
+    """Calculates nearest neighbors of target points in source points.
 
-    @abc.abstractmethod
-    def calc_error(self, point) -> float:
-        """Calculates error between data point and model.
-
-        :param point: data point to test against
-        """
+    :param src_mod: all points in source cloud after transformation SurfacePointCloud class from keypts.py
+    :param trg_pts: initial matches for target cloud SurfacePointCloud class from keypts.py
+    """
+    
+    tree = src_pts.kdtree
+    trg_xyz = trg_pts.points
+    
+    [k, idx, _] = tree.search_knn_vector_3d(trg_xyz, 1)
+    return idx
--- pyransac/ransac.py
+++ pyransac/ransac.py
@@ -1,17 +1,23 @@
 """Random sample consensus (RANSAC) module.
 
 This module contains the core code for the RANSAC algorithm.
+
+Originally written by Adam Morrissett
+
+Adapted by Eleanor Bash to filter 3D point cloud correspondences bsaed on 
+
+Persad, RA, and C. Armenakis (2017). Automatic 3D Surface Co-Registration 
+Using Keypoint Matching Photogrammetric Engineering & Remote Sensing, 83(2): 
+    137-151.
 """
 
 # Standard library imports
 from dataclasses import dataclass
 from math import log
 import random
-from typing import List
 
 # Local application imports
-from pyransac.base import Model
-
+import base
 
 @dataclass
 class RansacParams:
@@ -19,46 +25,56 @@ class RansacParams:
 
     This class contains the parameters for the RANSAC algorithm.
     """
-    samples: int
+    samples: 3
     """The number of random samples to take per iteration."""
 
     iterations: int
     """Maximum number iterations to complete."""
 
-    confidence: float
-    """The RANSAC confidence value (0 <= confidence <= 1)."""
+    # confidence: float
+    # """The RANSAC confidence value (0 <= confidence <= 1)."""
 
-    threshold: float
-    """The error threshold to consider a point an inlier"""
+    # threshold: float
+    # """The error threshold to consider a point an inlier"""
 
 
-def find_inliers(points: List, model: Model, params: RansacParams):
+def find_inliers(source,target,src_ind,trg_ind, params: RansacParams):
     """Find the inliers from a data set.
 
     Finds the inliers from a given data set given a model and
     an error function.
 
-    :param points: data points to evaluate
-    :param model: type of model to which the data should adhere
+    :param target: data points in desired reference frame SurfacePointCloud class from keypts.py
+    :param source: data points in reference frame to be transformed SurfacePointCloud class from keypts.py
+    :param src_ind: indices from cost matrix for source points
+    :param trg_ind: indices from cost matrix for target points
     :param params: parameters for the RANSAC algorithm
-    :return: inliers
+    :return: inliers, number of supporting points
     """
+    par_idx = numpy.arange(0,len(ptCloud.xyz))
     inliers = []
     max_support = 0
     iterations = params.iterations
     i = 0
 
     while i < iterations:
-        sample_points = random.choices(points, k=params.samples)
-        model.make_model(sample_points)
-        supporters = _find_supporters(points, model, params.threshold)
+        #adjust to select indices rather than points
+        sample_ind = random.choices(par_idx, k=params.samples)
+        
+        #find transform for this iteration
+        M = base.make_transform(sample_ind,source,target)
+        
+        #transform source points
+        model = source.transform_pts(M)
+        corres_t = target.point_cloud.select_down_sample(trg_ind)
+        supporters = find_supporters(corres_t,model,src_ind)
 
         if len(supporters) > max_support:
             max_support = len(supporters)
             inliers = supporters
 
             confidence = 1 - params.confidence
-            ratio = len(supporters) / len(points)
+            ratio = len(supporters) / len(trg_ind)
 
             # We cannot get more support than all data points
             if ratio == 1:
@@ -68,15 +84,17 @@ def find_inliers(points: List, model: Model, params: RansacParams):
 
         i += 1
 
-    return inliers
+    return inliers, max_support
 
 
-def _find_supporters(points: List, model: Model, threshold: float) -> List:
-    """Find data points (supporters) that support the given hypothesis.
+def find_supporters(corres_t, model, src_ind):
+    """Find data points (supporters) that support the given transformation.
 
-    :param points: data points to test against the hypothesis
-    :param model: type of model to which the data should adhere
-    :param threshold: error threshold to consider data point an inlier
+    :param cores_t: data points target point cloud correspondences
+    :param model: source point cloud transformed
+    :param src_ind: indices for points which are initial matches for corres_t
     :return: data points that support the hypothesis
     """
-    return [point for point in points if model.calc_error(point) <= threshold]
+    neighbors = base.calc_neighbors(model, corres_t) #indices of NN to each intial target cloud match
+    
+    return [point for point in neighbors if neighbors[point] == src_ind[point]]
